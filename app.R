cbdd54e38b136cb38a4858ac8baab6c130116cf4
JM Delgado <jmigueldelgado@posteo.net>
2021-02-16 22:01:07 +0100

    adds secavista and buhayra-app

--- app.R
+++ app.R
@@ -45,7 +45,27 @@ ui <- dashboardPage(
                                          href ="http://natriskchange.ad.umwelt.uni-potsdam.de:3838/HydroExplorer/")),
                tags$p("Source Code:", tags$a("https://github.com/ERottler/meltimr", 
                                          href ="https://github.com/ERottler/meltimr"))
-               ),
+             ),
+
+                          box(
+               title = "Seca Vista | Drought Forecase Ceara (Brazil)", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
+               
+               tags$p("SecaVista provides monthly meteorological and hydrological drought severity forecasts with a
+lead time of 8 months for the state of Ceará, Brazil. Meteorological drought severity is represented with the Standardized Precipitation Index (SPI) and with a Rainfall Anomaly (RA). Hydrological drought severity was defined as the mean storage level of reservoirs.
+The forecasts are based on an ensemble of the ECHAM4.6 Global Circulation Model (GCM) with
+20 ensemble members. Since the spatial resolution of ECHAM4.6 is too coarse to provide detailed information for the state of Ceará, and since such atmospheric models are usually systematically biased compared to ground observations, we apply Empirical Quantile Mapping (EQM)
+to adjust the ECHAM4.6 ensemble to the local scale using observations from a meteorological station network. EQM improves systematic biases throughout the statistical distribution of
+the variables modelled by the ECHAM4.6 model (i.e. temperature and precipitation) by training
+mapping functions based on the empirical cumulative distributions of the observed and modelled variables for each meteorological station in hindcast mode and then applied to forecasts of 8-month lead time. This way, forecasts of the ECHAM4.6-ensemble are downscaled to the
+local meteorological network."),
+               
+               tags$p("Web App:", tags$a("http://seca-vista.geo.uni-potsdam.de:3838/seca-vista-app/", 
+                                         href ="http://seca-vista.geo.uni-potsdam.de:3838/seca-vista-app/")),
+               tags$p("Research Article:", tags$a("https://hess.copernicus.org/articles/22/5041/2018/", 
+                                                href ="https://hess.copernicus.org/articles/22/5041/2018/"))
+               
+             ),
+
              
              box(
                title = "Add Title", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
@@ -59,17 +79,6 @@ ui <- dashboardPage(
       #right column of the Shiny dashboard
       column(width = 6,
              
-             box(
-               title = "Seca Vista | Drought Forecase Ceara (Brazil)", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
-               
-               tags$p("Information on Seca Vista"),
-               
-               tags$p("Web App:", tags$a("http://seca-vista.geo.uni-potsdam.de:3838/seca-vista-app/", 
-                                         href ="http://seca-vista.geo.uni-potsdam.de:3838/seca-vista-app/")),
-               tags$p("Research Article:", tags$a("https://hess.copernicus.org/articles/22/5041/2018/", 
-                                                href ="https://hess.copernicus.org/articles/22/5041/2018/"))
-               
-             ),
              
              box(
                title = "Avalanche | NatRiskChange Task Force Report", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
@@ -90,11 +99,34 @@ ui <- dashboardPage(
              ),
              
              box(
+               title = "Buhayra | A prototype product for presenting distributed water storage in a large reservoir network", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
+               
+               tags$p("Due to frequent cloud cover, continuous monitoring using optical satellite data does not achieve
+complete coverage of dense reservoir networks typical of semi-arid regions. Only synthetic aperture radar (SAR) remote sensing systems like Sentinel-1 enable a consistent detection through
+clouds, and with some limitations, through rainfall. Since the launch of Sentinel-1 mission, SAR
+data with reasonable resolution is available through ESA’s API with low latency. This enables the
+frequent and efficient retrieval of SAR data for large swaths of land surface. After data retrieval,
+reservoirs must be classified, georeferenced and collected into a geospatial database.
+Buhayra is a prototype application for obtaining water extent of small reservoirs in semi-arid
+regions from satellite data in real-time. It collects, filters and processes weekly reservoir extents
+from Sentinel-1 for northeast Brazil and stores this geo-referenced information in a structured
+data model. Since August 2020 a demo application runs in a test server.
+The current database schema enables geospatial queries by a map-server. The map-server can
+efficiently render large amounts of watermasks and serve them to a web application."),
+               tags$p("Dashboard:", tags$a("https://seca-vista.geo.uni-potsdam.de/buhayra-app/", 
+                                         href ="https://seca-vista.geo.uni-potsdam.de/buhayra-app/")),
+               tags$p("Source Code:", tags$a("https://github.com/jmigueldelgado/buhayra-app", 
+                                             href ="https://github.com/jmigueldelgado/buhayra-app"))
+
+
+             ),
+
+             box(
                title = "Add Title", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
                
                tags$p("Add description")
                )
-      ),
+       ),
       
       column(width = 12,
              
