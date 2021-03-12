4f33d9839a2917744e3d1d59c6638c1bb5d74134
ERottler <rottler@uni-potsdam.de>
2021-03-12 16:01:18 +0100

    Update dashboard layout.

--- app.R
+++ app.R
@@ -11,11 +11,11 @@ library(shinyWidgets)
 
 ui <- dashboardPage(
   
-  skin = "black",
+  # skin = "black",
   
   
-  dashboardHeader(title = "Inte-R-active Environmental Sciences",
-                  titleWidth = 450
+  dashboardHeader(title = "Inte-R-active Environmental Sciences - Potsdam/Berlin",
+                  titleWidth = 550
   ),
   
   dashboardSidebar(disable = TRUE),
@@ -23,61 +23,94 @@ ui <- dashboardPage(
   dashboardBody(
     
     tags$head(tags$style(HTML('
-
+                         
+      /* logo */
+      .skin-blue .main-header .logo {
+      background-color: #006699;
+      }
+                              
+      /* logo when hovered */
+      .skin-blue .main-header .logo:hover {
+      background-color: #006699;
+      }
+                              
+      /* navbar (rest of the header) */
+      .skin-blue .main-header .navbar {
+      background-color: #006699;
+      }
+                              
       .main-header .logo {
       font-family: sans-serif;
       font-weight: bold;
-      font-size: 18px;
+      font-size: 16px;
       }
 
       /* body */
       .content-wrapper, .right-side {
-      background-color: lightgrey;
+      background-color: #FFFFFF;
       text-align: justify;
       font-family:  sans-serif;
+      font-size: 14px;
       }
 
       .box.box-solid.box-primary>.box-header {
-      color:#fff;
-      background:#999999
+      color:#000000;
+      background:#CCCCCC
       }
 
       .box.box-solid.box-primary{
-      border-bottom-color:#333333;
-      border-left-color:#333333;
-      border-right-color:#333333;
-      border-top-color:#333333;
-      background:#999999
+      border-bottom-color: #000000;
+      border-left-color:#000000;
+      border-right-color:#000000;
+      border-top-color:#000000;
+      background:#FFFFFF
+      }
+
+      .box.box-solid.box-info>.box-header {
+      color:#FFFFFF;
+      background:#006699
+      }
+
+      .box.box-solid.box-info{
+      border-bottom-color: #000000;
+      border-left-color:#000000;
+      border-right-color:#000000;
+      border-top-color:#000000;
+      background:#FFFFFF
       }
 
     '))),
     
     
-    tags$head(tags$style(HTML("a {color: #336666}"))),
+    tags$head(tags$style(HTML("a {color: #006699}"))),
+    
+
+    tags$h3("A short overview on", align = "center"),
+    tags$h1("scientifically engineed applications, maps, graphs,...using", tags$em(tags$b("R")), align = "center", style="color: #006699"),
+    
+    br(),
+    
+    tags$h4("In recent years, the potential of interactive web tools to share information is increasingly recognized and used in the 
+             scientific community. Researchers need to further harness this potential and develop easy accessible online tools that 
+             can be used in- and outside the scientific community to explore, learn, teach and communicate environmental issues. 
+             In the following, an overview of scientifically engineered interactive tools developed by environmental researchers in Potsdam/Berlin
+             using", tags$a("R", href = "https://www.r-project.org/"), ". This compilation of available software hopefully strengthens networks among researchers,
+             inspires new ideas and helps to build up supportive structures for researchers interested in developing interactive applications.", align = "center"),
+    
+    br(),
+    br(),
     
     fluidRow(
       
-      #Intro accross entire dashbboard
-      column(width = 12,
-             box(
-               title = "Scentifically engineered applications, maps, graphs,...", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL,
-               tags$p("In recent years, the potential of interactive web tools to share information is increasingly recognized and used in the 
-                       scientific community. Researchers need to further harness this potential and develop easy accessible online tools that 
-                       can be used in- and outside the scientific community to explore, learn, teach and communicate environmental issues. 
-                       In the following, an overview of scientifically engineered interactive tools developed by environmental researchers in Potsdam/Berlin
-                       using", tags$a("R", href = "https://www.r-project.org/"), "."
-                      )
-               )
-      ),
       #left column of the Shiny dashbboard
       column(width = 6,
              box(
-               title = "Hydro Explorer | Changes in river runoff all over the world", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
+               title = "Hydro Explorer | Changes in river runoff all over the world", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                
                tags$p("The Hydro Explorer is an interactive web application that enables the investigation of of more than 7000 daily resolution discharge time
                       series form the Global Runoff Data Centre (GRDC) wiht regard to changs in runoff timing and ruoff seasonality. The web app allows for a 
                       quick comparison of gauges regions, methods and time frames. A detailed description using examples from the Rhine River basin is included into the", 
-                      tags$a("research artile", href = "https://doi.org/10.1002/rra.3772"), "pubblished in River Reserach and Applications."
+                      tags$a("research article", href = "https://doi.org/10.1002/rra.3772"), "pubblished in River Reserach and Applications."
                       ),
                tags$p("Web App:", tags$a("http://natriskchange.ad.umwelt.uni-potsdam.de:3838/HydroExplorer/", 
                                          href ="http://natriskchange.ad.umwelt.uni-potsdam.de:3838/HydroExplorer/")),
@@ -86,7 +119,7 @@ ui <- dashboardPage(
              ),
              
              box(
-               title = "Avalanche | NatRiskChange Task Force Report", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL, collapsed = T,
+               title = "Avalanche | NatRiskChange Task Force Report", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                
                tags$p("In order to enable and enhace inderdisciplinary and inter-methodological team work, PhD studens of the DFG-funded
                       Research Training Group", tags$a("NatRiskChange", href = "https://www.uni-potsdam.de/en/natriskchange"),
@@ -109,16 +142,15 @@ ui <- dashboardPage(
       column(width = 6,
              
              box(
-               title = "Seca Vista | Drought Forecase Ceara (Brazil)", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL,
+               title = "Seca Vista | Drought Forecase Ceara (Brazil)", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                
-               tags$p("SecaVista provides monthly meteorological and hydrological drought severity forecasts with a
-lead time of 8 months for the state of Ceará, Brazil. Meteorological drought severity is represented with the Standardized Precipitation Index (SPI) and with a Rainfall Anomaly (RA). Hydrological drought severity was defined as the mean storage level of reservoirs.
-The forecasts are based on an ensemble of the ECHAM4.6 Global Circulation Model (GCM) with
-20 ensemble members. Since the spatial resolution of ECHAM4.6 is too coarse to provide detailed information for the state of Ceará, and since such atmospheric models are usually systematically biased compared to ground observations, we apply Empirical Quantile Mapping (EQM)
-to adjust the ECHAM4.6 ensemble to the local scale using observations from a meteorological station network. EQM improves systematic biases throughout the statistical distribution of
-the variables modelled by the ECHAM4.6 model (i.e. temperature and precipitation) by training
-mapping functions based on the empirical cumulative distributions of the observed and modelled variables for each meteorological station in hindcast mode and then applied to forecasts of 8-month lead time. This way, forecasts of the ECHAM4.6-ensemble are downscaled to the
-local meteorological network."),
+               tags$p("SecaVista provides monthly meteorological and hydrological drought severity forecasts with a lead time of 8 months for the state of Ceará, Brazil. Meteorological drought severity is represented with the Standardized Precipitation Index (SPI) and with a Rainfall Anomaly (RA). Hydrological drought severity was defined as the mean storage level of reservoirs.
+                       The forecasts are based on an ensemble of the ECHAM4.6 Global Circulation Model (GCM) with
+                       20 ensemble members. Since the spatial resolution of ECHAM4.6 is too coarse to provide detailed information for the state of Ceará, and since such atmospheric models are usually systematically biased compared to ground observations, we apply Empirical Quantile Mapping (EQM)
+                       to adjust the ECHAM4.6 ensemble to the local scale using observations from a meteorological station network. EQM improves systematic biases throughout the statistical distribution of
+                       the variables modelled by the ECHAM4.6 model (i.e. temperature and precipitation) by training
+                       mapping functions based on the empirical cumulative distributions of the observed and modelled variables for each meteorological station in hindcast mode and then applied to forecasts of 8-month lead time. This way, forecasts of the ECHAM4.6-ensemble are downscaled to the
+                       local meteorological network."),
                
                tags$p("Web App:", tags$a("http://seca-vista.geo.uni-potsdam.de:3838/seca-vista-app/", 
                                          href ="http://seca-vista.geo.uni-potsdam.de:3838/seca-vista-app/")),
@@ -128,20 +160,20 @@ local meteorological network."),
              ),
              
              box(
-               title = "Buhayra | A prototype product for presenting distributed water storage in a large reservoir network", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL, collapsed = T,
+               title = "Buhayra | A prototype product for presenting distributed water storage in a large reservoir network", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                
                tags$p("Due to frequent cloud cover, continuous monitoring using optical satellite data does not achieve
-complete coverage of dense reservoir networks typical of semi-arid regions. Only synthetic aperture radar (SAR) remote sensing systems like Sentinel-1 enable a consistent detection through
-clouds, and with some limitations, through rainfall. Since the launch of Sentinel-1 mission, SAR
-data with reasonable resolution is available through ESA’s API with low latency. This enables the
-frequent and efficient retrieval of SAR data for large swaths of land surface. After data retrieval,
-reservoirs must be classified, georeferenced and collected into a geospatial database.
-Buhayra is a prototype application for obtaining water extent of small reservoirs in semi-arid
-regions from satellite data in real-time. It collects, filters and processes weekly reservoir extents
-from Sentinel-1 for northeast Brazil and stores this geo-referenced information in a structured
-data model. Since August 2020 a demo application runs in a test server.
-The current database schema enables geospatial queries by a map-server. The map-server can
-efficiently render large amounts of watermasks and serve them to a web application."),
+                       complete coverage of dense reservoir networks typical of semi-arid regions. Only synthetic aperture radar (SAR) remote sensing systems like Sentinel-1 enable a consistent detection through
+                       clouds, and with some limitations, through rainfall. Since the launch of Sentinel-1 mission, SAR
+                       data with reasonable resolution is available through ESA’s API with low latency. This enables the
+                       frequent and efficient retrieval of SAR data for large swaths of land surface. After data retrieval,
+                       reservoirs must be classified, georeferenced and collected into a geospatial database.
+                       Buhayra is a prototype application for obtaining water extent of small reservoirs in semi-arid
+                       regions from satellite data in real-time. It collects, filters and processes weekly reservoir extents
+                       from Sentinel-1 for northeast Brazil and stores this geo-referenced information in a structured
+                       data model. Since August 2020 a demo application runs in a test server.
+                       The current database schema enables geospatial queries by a map-server. The map-server can
+                       efficiently render large amounts of watermasks and serve them to a web application."),
                tags$p("Dashboard:", tags$a("https://seca-vista.geo.uni-potsdam.de/buhayra-app/", 
                                          href ="https://seca-vista.geo.uni-potsdam.de/buhayra-app/")),
                tags$p("Source Code:", tags$a("https://github.com/jmigueldelgado/buhayra-app", 
@@ -149,25 +181,29 @@ efficiently render large amounts of watermasks and serve them to a web applicati
 
 
              )
-       ),
-                
-      
-      #Introduction box additional further tools, models, packages,...
-      column(width = 12,
-             
-             box(
-               title = "Further tools, models, packages,...", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = F,
-               tags$p("You want to get to know further tools, models, packages,...engineerd by environemntal researcher in Potsdam/Berlin? And not only based on R, but using
-                      Pyhton, Matlab,...? Then enjoy browsing through the following compilation.")
-             )
-             
+       )
       ),
+    
+    fluidRow(
+          
+      br(),
+      
+      tags$h3("and ", align = "center"),
+      tags$h1("further tools, models, packages based on Python, Matlab,...", align = "center", style="color: #006699"),
+      
+      br(),
+      
+      tags$h4("You want to get to know further tools, models, packages,...engineerd by environemntal researcher in Potsdam/Berlin? And not only based on R, but using
+                      Pyhton, Matlab,...? Then continue browsing through the following compilation.", align = "center"),
+      
+      br(),
+      br(),
       
       #rigth column further tools, models, packages,...
       column(width = 6,
              
              box(
-               title = "WASA-SED | Numerical Model for hydrological and sediment fluxes", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL, collapsed = T,
+               title = "WASA-SED | Numerical Model for hydrological and sediment fluxes", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                tags$p("WASA-SED is a numerical model that allows for the simulation of hydrological and sediment fluxes at the meso-scale enigeered at the University of Potsdam.
                       The continous, deterministic and spatially semi-distributed model originally was developed by",
                       tags$a("Guentner (2002)", href = "https://publishup.uni-potsdam.de/opus4-ubp/frontdoor/deliver/index/docId/59/file/guentner.pdf"),
@@ -183,7 +219,7 @@ efficiently render large amounts of watermasks and serve them to a web applicati
              ),
              
              box(
-               title = "TopoToolbbox | MATLAB-based software for topographic analysis", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL, collapsed = T,
+               title = "TopoToolbbox | MATLAB-based software for topographic analysis", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                tags$p("TopoToolbox provides a set of MATLAB functions that support the analysis of relief and flow pathways in digital elevation models. The major aim of TopoToolbox 
                       is to offer helpful analytical GIS utilities in a non-GIS environment in order to support the simultaneous application of GIS-specific and other quantitative methods."),
                tags$p("Website:", tags$a("https://topotoolbox.wordpress.com/", href = "https://topotoolbox.wordpress.com/")),
@@ -192,11 +228,12 @@ efficiently render large amounts of watermasks and serve them to a web applicati
              
       ),
       
+      
       #right column further tools, models, packages,...
       column(width = 6,
              
              box(
-               title = "rainymotion | Python library for radar-based precipitation nowcasting", collapsible = T,  status = NULL, solidHeader = TRUE,  width = NULL, collapsed = T,
+               title = "rainymotion | Python library for radar-based precipitation nowcasting", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
                tags$p("Precipitation nowcasting has become an essential technique in various application context, such as early warming
                       or urban sewage control. The Python library rainymotion serves as tool for fast, free, and transparent precipitation nowcasting, is based only on free and open source software,
                       and can serve as a benchmark for further model development and hypothesis testing."),
@@ -205,13 +242,15 @@ efficiently render large amounts of watermasks and serve them to a web applicati
              )
       ),
       
+      br(),
+      
       #Contact information accross entire dashboard
       column(width = 12,
              
              box(
-               title = "Contact", collapsible = T,  status = "primary", solidHeader = TRUE,  width = NULL, collapsed = T,
+               title = "Contact", collapsible = T,  status = "info", solidHeader = TRUE,  width = NULL, collapsed = T,
                tags$p("Should you have any questions suggestions or need more information, please do not hesitate to contact
-                      the developers of the individual tools. Should you have questions regarding this Shiny dashbboard,
+                      the developers of the individual tools. Should you have questions regarding this Shiny dashbboard or want to contribute,
                       please vsit the", tags$a("repository", href = "https://github.com/ERottler/env-sci-interactive"),
                       "of this project or write an email to rottler(a)uni-potsdam.de or martinsd(a)uni-potsdam.de")
              )
